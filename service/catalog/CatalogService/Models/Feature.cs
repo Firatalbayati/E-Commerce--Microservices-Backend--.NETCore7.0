b1dc20f8ff639119053633ca9586446e997aa521
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2022-08-28 21:28:17 +0300

    Add project files.

---
+++ service/catalog/CatalogService/Models/Feature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Models
+{
+    public class Feature
+    {
+        public int Duraction { get; set; } 
+    }
+}
