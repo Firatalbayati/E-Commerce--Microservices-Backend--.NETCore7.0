174b704bf7ca664497192f08be0039571bc5b526
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-02-13 01:25:44 +0300

    Create Basket.API

---
+++ Services/Basket/MyCourse.Service.Basket/Services/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using MyCourse.Service.Basket.Dtos;
+using MyCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCourse.Service.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+
+        Task<Response<bool>> Delete(string userId);
+    }
+}
---
+++ Services/Basket/MyCourse.Service.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCourse.Service.Basket.Settings
+{
+    public class RedisSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
