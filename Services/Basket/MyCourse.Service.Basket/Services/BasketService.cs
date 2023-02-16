01081d4980b9f71985bc5c40284a5c2808661f3e
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-02-16 13:18:48 +0300

    BasketService class

---
+++ Services/Basket/MyCourse.Service.Basket/Services/BasketService.cs
@@ -0,0 +1,45 @@
+﻿using MyCourse.Service.Basket.Dtos;
+using MyCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyCourse.Service.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<Response<bool>> Delete(string userId)
+        {
+            var status = await _redisService.GetDB().KeyDeleteAsync(userId);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket Not Found", 404);
+        }
+
+        public async Task<Response<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDB().StringGetAsync(userId);
+
+            if(String.IsNullOrEmpty(existBasket))
+            {
+                return Response<BasketDto>.Fail("Basket Not Found", 404);
+            }
+
+            return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200);
+        }
+
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDB().StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket Could Not Update Or Save", 500);
+        }
+    }
+}
