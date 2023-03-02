425ed2b443782740b2b2ebe9cf585ad4671cf1e9
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-03-02 21:03:26 +0300

    Create (Order & OrderItem & Address) Classes

---
+++ Services/Order/MyCourse.Service.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,39 @@
+﻿using MyCourse.Service.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCourse.Service.Order.Domain.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreateDate { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId, Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreateDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GatTotalPrice => _orderItems.Sum(x => x.Price);
+    }
+}
