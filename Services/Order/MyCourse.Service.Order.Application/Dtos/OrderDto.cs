e6196d0419097c035989aac3db5c28647f3cd707
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-03-04 15:39:46 +0300

    Create Dtos

---
+++ Services/Order/MyCourse.Service.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCourse.Service.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public AddressDto Address { get; set; }
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+}
