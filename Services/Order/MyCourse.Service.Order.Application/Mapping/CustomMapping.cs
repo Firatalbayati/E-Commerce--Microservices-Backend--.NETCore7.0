66c0b998e5a2debe9dd7bd091b3ddb8a898fde2d
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-03-04 15:55:04 +0300

    AutoMapper

---
+++ Services/Order/MyCourse.Service.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MyCourse.Service.Order.Application.Dtos;
+using MyCourse.Service.Order.Domain.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCourse.Service.Order.Application.Mapping
+{
+    internal class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
+    }
+}
