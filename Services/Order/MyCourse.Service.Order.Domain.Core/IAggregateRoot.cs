24b05bcb5eb7cc92e99a2368f1d258e4dc644f0e
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-03-04 15:30:22 +0300

    OrderDbContext

--- Services/Order/MyCourse.Service.Order.Domain.Core/IAggregateRoot.cs
+++ Services/Order/MyCourse.Service.Order.Domain.Core/IAggregateRoot.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MyCourse.Service.Order.Domain.Core
 {
--- Services/Order/MyCourse.Service.Order.Domain/OrderAggregate/Address.cs
+++ Services/Order/MyCourse.Service.Order.Domain/OrderAggregate/Address.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace MyCourse.Service.Order.Domain.OrderAggregate
 {
     public class Address : ValueObject
