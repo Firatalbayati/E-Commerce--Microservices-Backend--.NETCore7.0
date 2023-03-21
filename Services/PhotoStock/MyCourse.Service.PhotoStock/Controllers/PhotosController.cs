af3e914524f1dea268c5f64aced2621de2480c0b
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-03-21 21:27:48 +0300

    PhotosController Update

--- Services/PhotoStock/MyCourse.Service.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/MyCourse.Service.PhotoStock/Controllers/PhotosController.cs
@@ -27,7 +27,7 @@ namespace MyCourse.Service.PhotoStock.Controllers
                 using var stream = new FileStream(path, FileMode.Create);
                 await photo.CopyToAsync(stream, cancellationToken);
 
-                var returnPath = "photos/" + photo.FileName;
+                var returnPath = photo.FileName;
 
                 PhotoDto photoDto = new() { Url = returnPath };
                 return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
